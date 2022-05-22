adbb71d4d5f52fe77c3495a2d35613c3d5ea11f3
Anna Avtoneeva <nuta43@mail.ru>
2022-05-22 15:29:09 +0300

    first commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,9 +1,26 @@
+"use strict"
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr=[];
+  let d=(b**2)-(4 * a * c); 
+let x, x1;
+  if (d > 0) {
+x=(-b + Math.sqrt(d) )/(2*a);
+x1=(-b - Math.sqrt(d) )/(2*a);
+arr.push(x.toFixed(0));
+arr.push(x1.toFixed(0));
+console.log ("2 корня: "+ arr);
 }
-
+else if (d === 0) {
+x=-b/(2*a);
+arr.push(x.toFixed(0));
+console.log ("1 Корень:"+ arr);
+}
+else {
+  console.log("Дискриминант:"+d+" Корней нет");
+  console.log(arr);
+}
+return arr
+};
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
