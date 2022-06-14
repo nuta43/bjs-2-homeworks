eb24ef94d545a7ad97f634eb133ac64c2251d719
Anna Avtoneeva <nuta43@mail.ru>
2022-06-14 21:18:07 +0300

    fourth commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,41 @@
-function Student(name, gender, age) {
-    // Ваш код
-
-}
-
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
-}
-
-// ваш код для остальных методов
+function Student (name,gender,age) {
+  this.name=name;
+  this.gender=gender;
+  this.age=age;
+    
+  }
+  Student.prototype.setSubject = function (subjectName) {
+  this.subject=subjectName
+  }
+  
+  Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){ 
+    this.marks=[]
+     this.marks.push(mark)
+    
+  }
+   else {
+        this.marks.push(mark)
+      }
+  }
+  
+  
+      Student.prototype.addMarks = function (mark1,mark2,mark3,...rest) {
+        this.marks=[]
+        this.marks.push(mark1,mark2,mark3,...rest)
+        
+    }
+  Student.prototype.getAverage=function (arr) {
+    let sum=0
+    for (i=0;i<this.marks.length;i++){
+    sum+=this.marks[i]
+    }
+    return sum/this.marks.length
+  }
+  Student.prototype.exclude = function (reason) {
+  this.excluded=reason
+  
+    delete this.subject
+  delete this.marks
+    
+    }
