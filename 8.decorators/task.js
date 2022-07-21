16e625ab6ba9b4b3c274573c4b8be047ed64382d
Anna Avtoneeva <nuta43@mail.ru>
2022-07-21 20:57:31 +0300

    decorator

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,52 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+  function wrapper(...args) {
+      const hash =  args.join(','); 
+      let objectInCache = cache.find((item) => item === hash);
+      if (objectInCache) { 
+          objectInCache = func.call(this, ...args);
+          console.log("Из кэша: " + objectInCache); 
+          return "Из кэша: " + objectInCache;
+      }
+
+      let result = func(...args); 
+      cache.push(hash) ; 
+      if (cache.length > 5) { 
+        cache.shift  
+      }
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;  
+  }
+  return wrapper;
 }
-
-
+  
+  
 function debounceDecoratorNew(func) {
-  // Ваш код
+  let timerCall;
+  return function(...args) {
+    if (timerCall === undefined) {
+        timerCall = Date.now();
+        func.apply(this, args);
+      } else {
+        let Time = Date.now() - timerCall;
+        if (Time >= ms) {
+            timerCall = Date.now();
+            func.apply(this, args);
+        }
+    }
+}
 }
 
 function debounceDecorator2(func) {
-  // Ваш код
+  function wrapper (...args) {
+    wrapper.history.push(args)
+    wrapper.count++
+     return func.call(this, ...args)
+  }
+      wrapper.count = 0;
+  wrapper.history = [];
+  return wrapper;
 }
+  
+
