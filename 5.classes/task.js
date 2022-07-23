274bd9e5180a11cda18f2a4aef57e0ab8a204104
Anna Avtoneeva <nuta43@mail.ru>
2022-07-23 12:07:56 +0300

    commit

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -1,15 +1,12 @@
 class PrintEditionItem {
 
-  type=null
-  
-
-  
-  constructor (name,releaseDate,pagesCount,state=100) {
-    this.name=name
-    this.releaseDate=releaseDate
-    this.pagesCount=pagesCount;
-    this.state=state;
-  }
+  constructor (name, releaseDate, pagesCount) {
+    this.name = name;
+    this.releaseDate = releaseDate;
+    this.pagesCount = pagesCount;
+    this.state = 100;
+    this.type = null;
+}
   set state(value) {
         if (value<0) {
           this._state=0
@@ -37,54 +34,65 @@ get State () {
   }
 
 class Magazine extends PrintEditionItem {
-  constructor (name,releaseDate,pagesCount,state) {
-    super(name,releaseDate,pagesCount,state)
+  constructor (name,releaseDate,pagesCount) {
+    super(name,releaseDate,pagesCount)
   this.type="magazine";
   
 }
 }
 class Book extends PrintEditionItem {
- constructor (author,name,releaseDate,pagesCount,state) {
-    super(name,releaseDate,pagesCount,state)
+ constructor (author,name,releaseDate,pagesCount) {
+    super(name,releaseDate,pagesCount)
    this.type="book"
    this.author=author
   
 }
 }
 class NovelBook extends Book {
-  constructor (name,releaseDate,pagesCount,state) {
-    super(name,releaseDate,pagesCount,state)
+  constructor (author,name,releaseDate,pagesCount) {
+    super(author,name,releaseDate,pagesCount)
   this.type="novel";
   
   }
 }
 class FantasticBook extends Book {
-  constructor (name,releaseDate,pagesCount,state) {
-    super(name,releaseDate,pagesCount,state)
+  constructor (author,name,releaseDate,pagesCount) {
+    super(author,name,releaseDate,pagesCount)
   this.type="fantastic";}
 
 }
   class DetectiveBook extends Book {
-  constructor (name,releaseDate,pagesCount,state) {
-    super(name,releaseDate,pagesCount,state)
+  constructor (author,name,releaseDate,pagesCount) {
+    super(author,name,releaseDate,pagesCount)
   this.type="detective";
   }
   }
 
+  class Library {
+    constructor(name,books) { 
+      this.name = name
+      this.books = []
+    } 
 
-class Library {
-constructor (name) {
-  this.name=name
-  this.books=[]
-}
 
 addBook(book) {
-  if (this.books===undefined) {
-   this.books=[]
-  }
-  else if (this.state>30) {
-    this.books.push(book)
-  }
+  if(book.state > 30) {
+    this.books.push(book);
 }
-}
+return this.books;
+} 
 
+findBookBy(type, value) {
+  for (let i = 0; i < this.books.length; i++) {
+    if(this.books[i][type] === value) 
+    return this.books[i];
+}
+return null;
+}
+giveBookByName(bookName) {
+  for (let i = 0; i < this.books.length; i++) {
+    if (this.books[i].name === bookName) return this.books.splice([i],1)[0];
+}
+return null;
+}
+  }
